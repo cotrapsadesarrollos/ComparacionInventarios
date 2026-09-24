4af46bdcf628ef2c98f9025a2e6037563370cb6f
cotrapsadesarrollos <cotrapsa.desarrollos@gmail.com>
2026-09-24 15:16:22 -0600

    Update ComparacionInventarios.py

--- ComparacionInventarios.py
+++ ComparacionInventarios.py
@@ -8,16 +8,18 @@ import streamlit as st
 
 st.title("Comparacion de Inventarios", text_alignment="center")
 
+st.divider()
 st.header("Archvios necesarios para el procesamiento:")
 inventario_plataforma = st.file_uploader("Seleccionar Inventario de la Plataforma:", type=["xlsx","xls"])
 inventario_sae = st.file_uploader("Seleccionar Inventario de SAE:", type=["xlsx","xls"])
 #bases = st.file_uploader("Seleccionar Carpeta con los Inventarios de las Bases:", type=["xlsx","xls"], accept_multiple_files="directory")
 
+st.divider()
 numero_de_bases = st.number_input("Cuantas bases se encuentran activas?", min_value=1, max_value=10)
 dictio_bases = {}
 for i in range(numero_de_bases):
     base = st.file_uploader(f"Seleccionar Inventario de la Base {i+1}:", type=["xlsx","xls"], key=f"Base_{i}")
     if base is not None:
-        dictio_bases[base.name.split(".")[0]] = base
+        dictio_bases[base.name.split(".")[0]] = pd.read_excel(base)
         st.write(f"Inventario de {base.name.split(".")[0]} subido correctamente")
         base = None

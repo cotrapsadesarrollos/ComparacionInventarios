eae1465c1300b32de417f104293c14f856348ffc
cotrapsadesarrollos <cotrapsa.desarrollos@gmail.com>
2026-09-24 14:45:45 -0600

    Update file uploader prompts for clarity

--- ComparacionInventarios.py
+++ ComparacionInventarios.py
@@ -11,11 +11,11 @@ st.title("Comparacion de Inventarios", text_alignment="center")
 st.header("Archvios necesarios para el procesamiento:")
 inventario_plataforma = st.file_uploader("Seleccionar Inventario de la Plataforma:", type=["xlsx","xls"])
 inventario_sae = st.file_uploader("Seleccionar Inventario de SAE:", type=["xlsx","xls"])
-bases = st.file_uploader("Seleccionar Inventario de SAE:", type=["xlsx","xls"], accept_multiple_files="directory")
+bases = st.file_uploader("Seleccionar Carpeta con los Inventarios de las Bases:", type=["xlsx","xls"], accept_multiple_files="directory")
 
 numero_de_bases = st.number_input("Cuantas bases se encuentran activas?", min_value=1, max_value=10)
 dictio_bases = {}
 for i in range(numero_de_bases):
     dictio_bases["inventario_base_{0}".format(i)] = st.file_uploader(f"Seleccionar Inventario de la Base {i+1}:", type=["xlsx","xls"], key=f"Base_{i}")
 
-st.header(str(inventario_sae.name))
+#st.header(str(inventario_sae.name))

4b08f3fb0cadb1eaeece99b82f636bf3000ec851
cotrapsadesarrollos <cotrapsa.desarrollos@gmail.com>
2026-09-24 13:17:00 -0600

    Add inventory comparison Streamlit app setup

---
+++ ComparacionInventarios.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import openpyxl
+import os
+from pathlib import Path
+from openpyxl.utils.dataframe import dataframe_to_rows
+import streamlit as st
+
+st.title("Comparacion de Inventarios", text_alignment="center")
+
+st.header("Archvios necesarios para el procesamiento:")
+inventario_plataforma = st.file_uploader("Seleccionar Inventario de la Plataforma:", type=["xlsx","xls"])
+inventario_sae = st.file_uploader("Seleccionar Inventario de SAE:", type=["xlsx","xls"])
+
+numero_de_bases = st.number_input("Cuantas bases se encuentran activas?", min_value=1, max_value=10)
+dictio_bases = {}
+for i in range(numero_de_bases):
+    dictio_bases["inventario_base_{0}".format(i)] = st.file_uploader(f"Seleccionar Inventario de la Base {i+1}:", type=["xlsx","xls"], key=f"Base_{i}")
